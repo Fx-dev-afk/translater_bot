7555cf3fec73337a041ae73f9797a2b7dc981197
evilnotlive <62100314+Fx-dev-afk@users.noreply.github.com>
2022-09-24 18:15:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import pymysql
 
-TOKEN = '5359023166:AAEIiGztL2jSgCqAGTcYKt35cko4HneeMLI'
+TOKEN = ''
 STARTMSG = "Hi, i am translator bot. I use google python and google API. To change lang use /choose"
 CHOSEMSG = "Choose lang"
 LANGUES = ['ru', 'de', 'en']
